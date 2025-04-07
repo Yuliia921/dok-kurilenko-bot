8b0f8e1553b7e10d7f79a038672a09c902d3c6b6
Yuliia921 <yuliya-durnina@yandex.ru>
2025-04-07 17:06:18 +0300

    Add files via upload

--- generate_pdf.py
+++ generate_pdf.py
@@ -1,13 +1,9 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
 import os
 from datetime import datetime
 
 def generate_pdf(fields: dict) -> str:
-    pdfmetrics.registerFont(TTFont("DejaVuSans", "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"))
-
     fio = fields.get("ФИО", "consultation").replace(" ", "_")
     date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     filename = f"{fio}_{date}.pdf"
@@ -16,14 +12,14 @@ def generate_pdf(fields: dict) -> str:
     path = os.path.join("tmp", filename)
 
     c = canvas.Canvas(path, pagesize=A4)
-    c.setFont("DejaVuSans", 14)
+    c.setFont("Helvetica", 14)
     width, height = A4
 
     c.drawCentredString(width / 2, height - 50, "Консультативное заключение")
     c.line(50, height - 55, width - 50, height - 55)
 
     y = height - 90
-    c.setFont("DejaVuSans", 12)
+    c.setFont("Helvetica", 12)
     for k, v in fields.items():
         c.drawString(70, y, f"{k}: {v}")
         y -= 25
@@ -31,7 +27,7 @@ def generate_pdf(fields: dict) -> str:
     y -= 10
     c.drawString(70, y, "Врач акушер-гинеколог Куриленко Юлия Сергеевна")
     y -= 20
-    c.setFont("DejaVuSans", 10)
+    c.setFont("Helvetica", 10)
     c.drawString(70, y, "📞 +37455987715")
     y -= 15
     c.drawString(70, y, "Telegram: https://t.me/doc_Kurilenko")
